a0cf5df9548a8135ae1a14a99cbc2aa4cdfea0e9
anilkumar <anilkumarsahoo17@gmail.com>
2020-01-12 20:15:04 +0530

    initial commit

---
+++ src/main/java/com/example/aniljava/AniljavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.aniljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniljavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniljavaApplication.class, args);
+
+        System.out.println("hii test");
+    }
+
+}
---
+++ src/test/java/com/example/aniljava/AniljavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aniljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniljavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
